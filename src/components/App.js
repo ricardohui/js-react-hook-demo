da5b1462614705ee7ab32c37bd1f00c76feed544
Ricardo <eehaha@gmail.com>
2019-02-19 08:01:23 +0800

    working hook demo that reuse logic,useState and useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-
+import ResourceList from "./ResourseList";
+import UserList from "./UserList";
 const App = () => {
   const [resource, setResource] = useState("posts");
 
   return (
     <div>
       <div>
+        <UserList />
         <button onClick={() => setResource("posts")}>Posts</button>
         <button onClick={() => setResource("todos")}>Todos</button>
       </div>
       {resource}
+      <ResourceList resource={resource} />
     </div>
   );
 };
