b1a2d17c0f2c7ab45da94533bd964ea61342d247
Ricardo <eehaha@gmail.com>
2019-02-19 08:01:44 +0800

    remove unused variable

--- src/components/ResourseList.jsx
+++ src/components/ResourseList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import useResources from "./useResources";
 const ResourceList = ({ resource }) => {
   const resources = useResources(resource);
